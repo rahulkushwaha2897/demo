83b76834becd50d1d987cd12e32a0d50ff51d0f5
rahulkushwaha2897 <76255247+rahulkushwaha2897@users.noreply.github.com>
2022-06-09 17:10:14 +0530

    Update 2_linking.sh

--- .platform/hooks/postdeploy/2_linking.sh
+++ .platform/hooks/postdeploy/2_linking.sh
@@ -19,8 +19,8 @@ cd  /var/www/html/public/
 rm -rf var
 ln -s /efs/storage/web-var  var 
 
-cd /var/www/html
+#cd /var/www/html
 #if [ -d "builds" ]
 #then
 #echo "Copying  build folders and files to classes"
-sudo cp -rf builds/* var/classes/
+#sudo cp -rf builds/* var/classes/
